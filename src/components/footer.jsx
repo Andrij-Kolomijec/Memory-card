ba3e9a72148e752828225081c9e5e93fb72378aa
Difficins <a.kolomijec@seznam.cz>
2023-12-19 14:45:11 +0100

    Added footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="sources">
+        <p>
+          Image by{" "}
+          <a href="https://unsplash.com/photos/brown-wooden-board-h0Vxgz5tyXA">
+            Keith Misner
+          </a>{" "}
+          from <a href="https://unsplash.com/">Unsplash</a>
+        </p>
+        <p>
+          Icons from <a href="https://pictogrammers.com/">Pictogrammers</a>
+        </p>
+      </div>
+      <a href="https://github.com/Andrij-Kolomijec">
+        <img class="github" src="/github.svg" />
+      </a>
+    </footer>
+  );
+}
